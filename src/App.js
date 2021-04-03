a40bc79c2ad93d38b912eaaac8dcae150495987a
jaehuyn-choi <jaehyun-choi@linecorp.com>
2021-04-03 21:21:07 +0900

    fix: 기본 프로젝트의 App.js와 App.css를 변경 (hook -> class)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import logo, {  } from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends React.Component {
+  constructor(prop) {
+    super(prop)
+    this.clickTabNumber = this.clickTabNumber.bind(this)
+    this.state = {
+      selectedTabNumber: 0
+    }
+  }
+
+  clickTabNumber(num) {
+    this.setState(
+      { selectedTabNumber: num }
+    )
+  }
+
+  render() {
+    return (
+      <header className='App-header'>
+        <h1>
+          <ul>
+            <li>
+              <button onClick={() => this.clickTabNumber(1)} className={`${this.state.selectedTabNumber === 1 ? 'selected' : ''}`}>
+                <img src={logo} className={`App-logo`} alt='logo'/>
+                All
+              </button>
+            </li>
+            <li>
+              <button onClick={() => this.clickTabNumber(2)} className={`${this.state.selectedTabNumber === 2 ? 'selected' : ''}`}>
+                <img src={logo} className={`App-logo`} alt='logo'/>
+                IMG
+              </button>
+            </li>
+            <li>
+              <button onClick={() => this.clickTabNumber(3)} className={`${this.state.selectedTabNumber === 3 ? 'selected' : ''}`}>
+                <img src={logo} className={`App-logo`} alt='logo'/>
+                VIDEO
+              </button>
+            </li>
+          </ul>
+        </h1>
       </header>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
